015040e1b3c49849dbb3eb979a97a6130fbb9805
Luan Nico <luanpotter28@hotmail.com>
2015-01-19 23:12:23 -0200

    Reset API

---
+++ src/main/java/br/com/dextra/pointz/models/ResetPeople.java
@@ -0,0 +1,26 @@
+package br.com.dextra.pointz.models;
+
+import io.yawp.repository.IdRef;
+import io.yawp.repository.actions.Action;
+import io.yawp.repository.actions.annotations.PUT;
+
+public class ResetPeople extends Action<Person> {
+
+	@PUT("reset")
+	public void reset() {
+		deleteTransactions();
+		deletePeople();
+	}
+
+	private void deletePeople() {
+		for (IdRef<Person> id : yawp(Person.class).ids()) {
+			id.delete();
+		}
+	}
+
+	private void deleteTransactions() {
+		for (IdRef<Transaction> id : yawp(Transaction.class).ids()) {
+			id.delete();
+		}
+	}
+}
